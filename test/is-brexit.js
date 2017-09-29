cda631fe034ba2a196ea5cf49d251daf95d60eed
Anatoliy Gatt <anatoliy.gatt@aol.com>
2017-09-29 21:45:59 +0200

    Replace functions with fat arrow functions

--- test/is-brexit.js
+++ test/is-brexit.js
@@ -1,10 +1,10 @@
 var assert = require('assert');
 var isBrexit = require('../lib/is-brexit');
 
-describe('is-brexit', function () {
-    describe('init', function () {
-        it('should always return true', function () {
-            assert.equal(isBrexit(), true, 'Function should always return true');
-        });
+describe('is-brexit', () => {
+  describe('init', () => {
+    it('should always return true', () => {
+      assert.equal(isBrexit(), true, 'Function should always return true');
     });
+  });
 });
