594d4eb24b0368e55c9a8a986c17481b000809f1
Anatoliy Gatt <anatoliy.gatt@aol.com>
2017-09-30 10:12:31 +0200

    Update assert message in test

--- test/is-brexit.js
+++ test/is-brexit.js
@@ -4,7 +4,7 @@ var isBrexit = require('../lib/is-brexit');
 describe('is-brexit', () => {
   describe('init', () => {
     it('should always return true', () => {
-      assert.equal(isBrexit(), true, 'Function should always return true');
+      assert.equal(isBrexit(), true, 'should never return false');
     });
   });
 });
